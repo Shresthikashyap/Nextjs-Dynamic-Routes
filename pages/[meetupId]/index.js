8c7906802da63d1aa634a764abe9c0b3eb93446e
Shresthi Kashyap <sk2929542@gmail.com>
2024-03-11 21:29:22 +0530

    show details page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,20 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 
 import React from 'react'
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
+  console.log(props)
+
+  if (!props.meetupData) {
+    return <p>Meetup not found</p>; // Handle case where meetupData is null
+  }
   return (
     <MeetupDetail
-       image="https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960"
-       title="eden garden"
-       address="georgia park street 5"
-       description="a new city in berlin"
+       image={props.meetupData.image}
+       title={props.meetupData.title}
+       address={props.meetupData.address}
+       description={props.meetupData.description}
     />
   )
 }
@@ -17,10 +23,15 @@ const MeetupDetails = () => {
 
 export async function getStaticPaths() {
   // Assuming you have a list of meetup IDs
-  const meetupIds = ['M1', 'M2', 'M3'];
 
-  const paths = meetupIds.map((meetupId) => ({
-    params: { meetupId }
+  const client = await MongoClient.connect('mongodb+srv://sk2929542:1234shit@cluster0.7ypej7u.mongodb.net/Meetups?retryWrites=true&w=majority&appName=Cluster0');
+  const db = client.db('meetupDB'); // Replace 'meetupDB' with your database name
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({},{_id:1}).toArray();
+
+  const paths = meetups.map((meetup) => ({
+    params: { meetupId: meetup._id.toString() }
   }));
 
   return { paths, fallback: false };
@@ -30,16 +41,24 @@ export async function getStaticProps(context) {
   // Assuming you have a function to fetch meetup data by ID
   const { params } = context;
   const meetupId = params.meetupId; // Extract meetup ID from params
+  console.log('meetup Id',meetupId)
 
+  const client = await MongoClient.connect('mongodb+srv://sk2929542:1234shit@cluster0.7ypej7u.mongodb.net/Meetups?retryWrites=true&w=majority&appName=Cluster0');
+  const db = client.db('meetupDB'); // Replace 'meetupDB' with your database name
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({_id: new ObjectId(meetupId)});
+  //client.close()
+  console.log('meetup data',selectedMeetup)
 
   return {
     props: {
-      meetupData:{
-        id:meetupId,
-        image:"https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960",
-        title:"eden garden",
-        address:"georgia park street 5",
-        description:"a new city in berlin"
+      meetupData: {
+        id:selectedMeetup._id.toString(),
+        image:selectedMeetup.image,
+        title:selectedMeetup.title,
+        address:selectedMeetup.address,
+        description:selectedMeetup.description
       }
     }
   };
