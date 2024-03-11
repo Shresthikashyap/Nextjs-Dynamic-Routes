7a452747c070635300b7b41622a7470281960662
Shresthi Kashyap <sk2929542@gmail.com>
2024-03-11 15:34:46 +0530

    Prerendering

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -13,4 +13,36 @@ const MeetupDetails = () => {
   )
 }
 
+// 
+
+export async function getStaticPaths() {
+  // Assuming you have a list of meetup IDs
+  const meetupIds = ['M1', 'M2', 'M3'];
+
+  const paths = meetupIds.map((meetupId) => ({
+    params: { meetupId }
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  // Assuming you have a function to fetch meetup data by ID
+  const { params } = context;
+  const meetupId = params.meetupId; // Extract meetup ID from params
+
+
+  return {
+    props: {
+      meetupData:{
+        id:meetupId,
+        image:"https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960",
+        title:"eden garden",
+        address:"georgia park street 5",
+        description:"a new city in berlin"
+      }
+    }
+  };
+}
+
 export default MeetupDetails
--- pages/index.js
+++ pages/index.js
@@ -2,45 +2,54 @@ import React from 'react';
 import MeetupList from '@/components/meetups/MeetupList';
 import Layout from '@/components/layout/Layout';
 
-const HomePage = () => {
+// Define dummyMeetups outside of the component
+const dummyMeetups = [
+  {
+    id: 'M1',
+    image: 'https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960',
+    title: 'First Meetup',
+    address: '123 Street, City A',
+    description: 'This is the first meetup.',
+  },
+  {
+    id: 'M2',
+    image: 'https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960',
+    title: 'Second Meetup',
+    address: '456 Road, City B',
+    description: 'This is the second meetup.',
+  },
+  {
+    id: 'M3',
+    image: 'https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960',
+    title: 'Third Meetup',
+    address: '789 Avenue, City C',
+    description: 'This is the third meetup.',
+  },
+  {
+    id: 'M4',
+    image: 'https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960',
+    title: 'Fourth Meetup',
+    address: '1011 Lane, City D',
+    description: 'This is the fourth meetup.',
+  }
+];
 
-    const dummyMeetups = [
-        {
-          id: 'M1',
-          image: 'https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960',
-          title: 'First Meetup',
-          address: '123 Street, City A',
-          description: 'This is the first meetup.',
-        },
-        {
-          id: 'M2',
-          image: 'https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960',
-          title: 'Second Meetup',
-          address: '456 Road, City B',
-          description: 'This is the second meetup.',
-        },
-        {
-            id: 'M3',
-            image: 'https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960',
-            title: 'Third Meetup',
-            address: '789 Avenue, City C',
-            description: 'This is the third meetup.',
-        },
-        {
-            id: 'M4',
-            image: 'https://www.telegraph.co.uk/content/dam/Travel/hotels/2023/july/Berlin%20Brandenburg%20gate%20getty.jpg?imwidth=960',
-            title: 'Fourth Meetup',
-            address: '1011 Lane, City D',
-            description: 'This is the fourth meetup.',
-          }
-      ];
+const HomePage = (props) => {
   return (
     <div>
-        
-            <MeetupList meetups={dummyMeetups}/>
-        
+      <MeetupList meetups={props.meetups}/>
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps(context) {
+
+  
+  return {
+    props: {
+      meetups: dummyMeetups
+    }
+  };
 }
 
-export default HomePage
+export default HomePage;
